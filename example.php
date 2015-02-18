e5b3308186220af38df6ca04e83426f231a85cce
vojtatranta <vojta.tranta@gmail.com>
2015-02-18 13:33:50 +0100

    Updated urls and example

--- example.php
+++ example.php
@@ -20,8 +20,8 @@ $item = array(
 $customer = array(
     'name' => 'Karel Holan',
     'email' => 'karel.holan@rychmat.euxx',
-    'personal_id' => '141013/0018',
-    'id_card_no' => '123456789',
+    'personal_id' => '890707/0029',
+    'id_card_no' => '100786676',
     'id_card_expiry_date' => '2024-04-24'
 );
 
@@ -39,11 +39,16 @@ $orderData = array(
     'delivery_carrier' => 1,
     'delivery_state' => 1,
     'cookie_id' => 'dh763jdkl8783jsldif783728jnkjdflfsd',
-    'ip_address' => '79.98.79.213'
+    'ip_address' => '79.98.79.213',
+    'paygate' => True, //pokud chcete použít platební bránu
+    'success_url' => 'http://vaseshop.cz/vase-success-url', //přesměrovací url při úspěšném zaplacení objednávky
+    'failure_url' => 'http://vaseshop.cz/vase-failure-url', //přesměrovací url při neúspěšném zaplacení objednávky
+    'callback_url' => 'http://vaseshop.cz/vase-callback-url', //url kam se mají posílat změny o stavu objednávky
 );
 
+
 try {
-    $api = new KupNajistoApi('username', 'password', 'http://knj.rychmat.eu/'); // https://app.kupnajisto.cz
+    $api = new KupNajistoApi('vas@email.cz', 'vaseheslo', 'https://app.kupnajisto.cz/');
 
     $response = $api->createOrder( $orderData );
     var_dump($response); // json response as assoc array
